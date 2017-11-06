2046975b117b1589c7da85a5dd91c61f61fb1857
Michael Klamkin <klamike@users.noreply.github.com>
2017-11-06 18:49:51 -0500

    Update graday.1h.sh

--- graday.1h.sh
+++ graday.1h.sh
@@ -5,12 +5,10 @@
 # <bitbar.author>Michael Klamkin</bitbar.author>
 # <bitbar.author.github>klamike</bitbar.author.github>
 # <bitbar.desc>Days Until Graduation for Staten Island Technical High School </bitbar.desc>
-
 gradate="1529568000" #datetime of graduation
-dayconst="86400" #seconds in a day for division
-
 ctime="$(date +%s)" #current time in seconds
 tdiff=$((gradate - ctime)) #time left until graduation in seconds
+dayconst="86400" #seconds in a day for division
 diffday=$((tdiff / dayconst + 1)) #
 
 echo "$diffday | font = courier size = 24" #menu bar display
@@ -18,3 +16,4 @@ echo "$diffday | font = courier size = 24" #menu bar display
 echo "---" #for bitbar menu bar
 
 echo "$diffday Days Until Graduation | font = SanFrancisco"
+echo "June 21, 2018  8:00 AM" #day of graduation displays in dropdown menu
